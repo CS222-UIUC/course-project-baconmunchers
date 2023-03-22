5862e922ed0047d1c5f8de2cc9962c593cea085b
Daniel Zhang <dze61624@gmail.com>
2023-03-22 17:42:24 -0500

    slight script changes

--- scraperbackend/scraperapp/scripts/build_database.py
+++ scraperbackend/scraperapp/scripts/build_database.py
@@ -7,6 +7,41 @@ import json
 # import mysql.connector
 
 # from get_sections_from_class import get_sections_from_class
+def get_class_links():
+	course_explorer = 'https://courses.illinois.edu'
+	year = '2023'
+	season = 'spring'
+
+	subjects_page_link = course_explorer + f'/schedule/{year}/{season}'
+	subjects_page = requests.get(subjects_page_link)
+
+	#Parse through the course subjects page for all subject page links
+	subjects_page_soup = BeautifulSoup(subjects_page.text, 'html.parser')
+	subjects_page_links = [str(i.get('href')) for i in subjects_page_soup.find_all('a')]
+	subject_links_regex = re.compile(f'/schedule/{year}/{season}/.*')
+	subject_links = [course_explorer + link for link in subjects_page_links if not re.match(subject_links_regex, link) is None]
+
+	#build dictionary of subject codes to lists of class linkes
+	classes = {}
+
+	for subject_page_link in subject_links:
+		subject_code = re.split(r'/', subject_page_link)[-1]
+		subject_page = requests.get(subject_page_link)
+		subject_page_soup = BeautifulSoup(subject_page.text, 'html.parser')
+		subject_page_link = [str(i.get('href')) for i in subject_page_soup.find_all('a')]
+		class_links_regex = re.compile(f'/schedule/{year}/{season}/.*/\d\d\d')
+		class_links = [course_explorer + link for link in subject_page_link
+							if not re.match(class_links_regex, link) is None]
+		classes[subject_code] = class_links
+
+
+	#write classes to CLassLinks.txt for later use
+	with open("ClassLinks.txt", 'w') as file:
+		file.write(f"{len(classes)}\n")
+		for subject_code in classes:
+			file.write(f"{subject_code} {len(classes[subject_code])}\n")
+			for link in classes[subject_code]:
+				file.write(f"{link}\n")
 
 def get_sections_from_class(link):
 	"""Get non-online non-asynchronous sections of a class, given course explorer url.
@@ -50,7 +85,8 @@ def input_into_SQLite_database(section):
 	input = ClassInfo(CRN=section[0],StartTime=section[1],EndTime=section[2],Days=section[3],Building=section[4],Room=section[5])
 	input.save()
 
-def run():	
+def run(): 
+	get_class_links()
 	classes = {}
 	# remove all existing class info objects
 	ClassInfo.objects.all().delete()
