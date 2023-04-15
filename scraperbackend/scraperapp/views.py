508b8ce53efd1b9d0a4c05b762011ee79604c1ec
kaushik327 <kaushik.r.varadharajan@gmail.com>
2023-04-15 00:02:20 -0500

    alert if no classes found in get_class

--- scraperbackend/scraperapp/views.py
+++ scraperbackend/scraperapp/views.py
@@ -25,52 +25,54 @@ class ClassInfoView(viewsets.ReadOnlyModelViewSet):
 
 
 def get_class(request):
+    if request.method != 'POST':
+        return render(request, 'get_class.html', {'form': ClassForm()})
     # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = ClassForm(request.POST)
-        if form.is_valid():
-            # print(set(list(ClassInfo.objects.values_list('Building', flat=True))))
-            SubjectCode = form.cleaned_data['SubjectCode']
-            CourseNumber = form.cleaned_data['CourseNumber']
-            CRN = form.cleaned_data['CRN']
-            StartTime = form.cleaned_data['StartTime']
-            EndTime = form.cleaned_data['EndTime']
-            Days = form.cleaned_data['Days'] 
-            Building = form.cleaned_data['Building'] 
-            Room = form.cleaned_data['Room']
-            info = ClassInfo.objects.all()
-            # print(CRN, StartTime, EndTime, Days, Building, Room)
-            if SubjectCode != "":
-                info = info.filter(SubjectCode__icontains=SubjectCode)
-            if CourseNumber != None:
-                info = info.filter(CourseNumber__icontains=str(CourseNumber))
-            if CRN != None:
-                info = info.filter(CRN__icontains=str(CRN))
-                # could change the database parsing to store CRN as an integer
-#TODO Decide on logic for when a course should be filtered based on start and end time,
-# Ex: if the start time entered is after the start time and before the end time of a class, should it be included 
-            if StartTime != "":
-                info = info.filter(StartTime__icontains=StartTime)
-            if EndTime != "":
-                info = info.filter(EndTime__icontains=EndTime)
-            if Days != "":
-                for Day in Days:
-                    info = info.filter(Days__icontains=Day)
-            if Building != "":
-                info = info.filter(Building__icontains=Building)
-            if Room != "":
-                info = info.filter(Room__icontains=Room)
 
-            return render(request, 'classes.html', {'info': info})
-
-            
+    # create a form instance and populate it with data from the request:
+    form = ClassForm(request.POST)
+    if not form.is_valid():
         # should add a 404 exception if the form is invalid
-    else:
-        form = ClassForm()
+        return render(request, 'get_class.html', {'form': form})
 
-    # get_class.html is a template, stored in the templates directory
-    return render(request, 'get_class.html', {'form': form})
+    # print(set(list(ClassInfo.objects.values_list('Building', flat=True))))
+    SubjectCode = form.cleaned_data['SubjectCode']
+    CourseNumber = form.cleaned_data['CourseNumber']
+    CRN = form.cleaned_data['CRN']
+    StartTime = form.cleaned_data['StartTime']
+    EndTime = form.cleaned_data['EndTime']
+    Days = form.cleaned_data['Days'] 
+    Building = form.cleaned_data['Building'] 
+    Room = form.cleaned_data['Room']
+    info = ClassInfo.objects.all()
+    # print(CRN, StartTime, EndTime, Days, Building, Room)
+    if SubjectCode != "":
+        info = info.filter(SubjectCode__icontains=SubjectCode)
+    if CourseNumber != None:
+        info = info.filter(CourseNumber__icontains=str(CourseNumber))
+    if CRN != None:
+        info = info.filter(CRN__icontains=str(CRN))
+        # could change the database parsing to store CRN as an integer
+    #TODO Decide on logic for when a course should be filtered based on start and end time,
+    # Ex: if the start time entered is after the start time and before the end time of a class, should it be included 
+    if StartTime != "":
+        info = info.filter(StartTime__icontains=StartTime)
+    if EndTime != "":
+        info = info.filter(EndTime__icontains=EndTime)
+    if Days != "":
+        for Day in Days:
+            info = info.filter(Days__icontains=Day)
+    if Building != "":
+        info = info.filter(Building__icontains=Building)
+    if Room != "":
+        info = info.filter(Room__icontains=Room)
+    
+    if not info:
+        messages.error(request, 'No classes found. Are you sure you typed everything in correctly?')
+        return render(request, 'get_class.html', {'form': form})
+    
+    return render(request, 'classes.html', {'info': info})
+
 
 
 def building_page(request):
