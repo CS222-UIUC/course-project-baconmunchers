9d0560933d794ab366d67778f890e56ef7031af9
kaushik327 <kaushik.r.varadharajan@gmail.com>
2023-04-20 13:41:14 -0500

    test that building and course pages give the right alerts

--- scraperbackend/scraperapp/tests.py
+++ scraperbackend/scraperapp/tests.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
-from .forms import ClassForm
-from .models import ClassInfo
+from .forms import ClassForm, BuildingForm
+from .models import ClassInfo   
+from django.test import Client
+from http import HTTPStatus
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 # Create your tests here.
 
 class ClassInfoTestCase(TestCase):
@@ -58,3 +65,41 @@ class ClassInfoTestCase(TestCase):
         query = ClassInfo.objects.filter(SubjectCode="CS")
         self.assertEqual(len(query), 3)
         
+class TestPages(TestCase):
+    
+    def test_building_exists(self):
+        driver = webdriver.Chrome()
+        driver.get("http://127.0.0.1:8000/get_building/")
+        driver.find_element(By.ID, 'id_Building').send_keys('Campus Instructional Facility')
+        driver.find_element(By.ID, 'id_Building').submit()
+        assert "Building page doesn't exist yet. Try Advanced Search!" not in driver.page_source
+        driver.close()
+
+    def test_building_doesnt_exist(self):
+        driver = webdriver.Chrome()
+        driver.get("http://127.0.0.1:8000/get_building/")
+        driver.find_element(By.ID, 'id_Building').send_keys('asdf')
+        driver.find_element(By.ID, 'id_Building').submit()
+        assert "Building page doesn't exist yet. Try Advanced Search!" in driver.page_source
+        driver.close()
+    
+
+    def test_courses_exist(self):
+        driver = webdriver.Chrome()
+        driver.get("http://127.0.0.1:8000/get_class/")
+        driver.find_element(By.ID, 'id_SubjectCode').send_keys('CS')
+        driver.find_element(By.ID, 'id_CourseNumber').send_keys('222')
+        driver.find_element(By.ID, 'id_CourseNumber').submit()
+        assert "No classes found. Are you sure you typed everything in correctly?" not in driver.page_source
+        driver.close()
+
+    def test_courses_dont_exist(self):
+        driver = webdriver.Chrome()
+        driver.get("http://127.0.0.1:8000/get_class/")
+        driver.find_element(By.ID, 'id_SubjectCode').send_keys('CS')
+        driver.find_element(By.ID, 'id_CourseNumber').send_keys('223')
+        driver.find_element(By.ID, 'id_CourseNumber').submit()
+        assert "No classes found. Are you sure you typed everything in correctly?" in driver.page_source
+        driver.close()
+    
+    
